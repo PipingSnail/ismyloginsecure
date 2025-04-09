a69c5ed532884855d3c51c855a39e4faee4675b3
ismyloginsecure <ismyloginsecure@sprezzaturra.com>
2025-04-09 17:47:31 +0100

    Updated github URL
    Updated with correct github URL for the project

--- isMyLoginSecureDesktopDemo/LicenceDialog.Designer.cs
+++ isMyLoginSecureDesktopDemo/LicenceDialog.Designer.cs
@@ -156,10 +156,10 @@ namespace isMyLoginSecureDesktopDemo
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Location = new System.Drawing.Point(16, 576);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(179, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(231, 13);
             this.linkLabel1.TabIndex = 10;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "https://opensource.org/license/MIT";
+            this.linkLabel1.Text = "https://github.com/PipingSnail/ismyloginsecure";
             // 
             // label6
             // 
